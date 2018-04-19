2b6da3eca3391abcc6b17cee26e0e8bfe02b98d0
joseph-tohdjojo <joseph.tohdjojo@gmail.com>
2018-04-18 23:35:38 -0400

    click

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import lottie from 'lottie-web'
 import data from './data.json'
 
+const element = document.getElementById('bm')
+
 var anim = lottie.loadAnimation({
   container: document.getElementById('bm'),
   renderer: 'svg',
-  loop: true,
+  loop: false,
   autoplay: true,
   animationData: data
 })
+
+element.addEventListener('click', ev => anim.play())
