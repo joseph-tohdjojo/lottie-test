ddf25f55706258ae5eef58b7e2ef9056efb4906f
joseph-tohdjojo <joseph.tohdjojo@gmail.com>
2018-04-18 23:50:56 -0400

    no loop

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import lottie from 'lottie-web'
 import data from './data.json'
 
 const element = document.getElementById('bm')
+const replay = document.getElementById('replay')
 
 var anim = lottie.loadAnimation({
-  container: document.getElementById('bm'),
+  container: element,
   renderer: 'svg',
   loop: false,
   autoplay: true,
   animationData: data
 })
 
-element.addEventListener('click', ev => anim.play())
+window.addEventListener('click', ev => anim.goToAndPlay(0, true))
+anim.addEventListener('complete', ev => {
+  replay.style.opacity = 1
+})
