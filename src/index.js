d9bbf6c80f910b5564dda4af6384fd80b302c5e1
Joseph Toh Djojo <jt641v@att.com>
2018-04-22 21:46:30 -0400

    different behavior

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import lottie from 'lottie-web'
 import data from './data.json'
 
 const element = document.getElementById('bm')
-const replay = document.getElementById('replay')
 
 var anim = lottie.loadAnimation({
   container: element,
   renderer: 'svg',
   loop: false,
-  autoplay: true,
+  autoplay: false,
   animationData: data
 })
 
-window.addEventListener('click', ev => anim.goToAndPlay(0, true))
-window.addEventListener('touchend', ev => anim.goToAndPlay(0, true))
-anim.addEventListener('complete', ev => (replay.style.opacity = 1))
+window.addEventListener('mousedown', ev => {
+  anim.setDirection(1)
+  anim.play()
+})
+window.addEventListener('mouseup', ev => {
+  anim.setDirection(-1)
+  anim.play()
+})
